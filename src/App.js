5914cba97ff232c2979d92dc980f9abe567344f8
Drake Deaton <drake.s.deaton@gmail.com>
2020-06-09 17:12:12 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from 'public/logo/SVGLogo.svg';
+import logo from './../public/logo/400dpiLogo.png';
+import logo2 from './../public/logo/SVGLogo.svg';
 import './App.css';
 
 class App extends React.Component {
-
     constructor(props) {
         super(props);
-
     }
+
+    // componentDidMount() {
+    //     const container = document.getElementById('container');
+    //     const inner = document.getElementById('inner');
+    //
+    //     const onMouseEnterHandler = function(event) {
+    //         update(event);
+    //     };
+    //     const onMouseLeaveHandler = function() {
+    //         inner.style = "";
+    //     };
+    //     const onMouseMoveHandler = function(event) {
+    //         if (isTimeToUpdate()) {
+    //             update(event);
+    //         }
+    //     };
+    //
+    //     container.onmouseenter = onMouseEnterHandler;
+    //     container.onmouseleave = onMouseLeaveHandler;
+    //     container.onmousemove = onMouseMoveHandler;
+    // }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
+            <div className="App" >
+                <header className="App-header" id={"container"}>
+                    <img  id={"inner"} src={logo} className="App-logo" alt="logo"/>
+                    {/*<svg id={"inner"} xmlns={logo2} width="100" height="100"/>*/}
+                    <p class={"text slogan"}>
                         Coming Soon
                     </p>
                 </header>
             </div>
         );
     }
+
 }
 
 export default App;
+
+
